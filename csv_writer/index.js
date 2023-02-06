eb753fc06fbe8b033675ab31879e0dd028e9ec53
vrnsky <vrnsky@protonmail.ch>
2023-02-06 21:44:30 +0800

    NodeJS A to Z. Add example of async reading line from console

--- csv_writer/index.js
+++ csv_writer/index.js
@@ -1,9 +1,24 @@
 import { writeFileSync } from "fs"
+import { createInterface } from "readline"
 
 const content = "Test content"
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const readLineAsync = message => {
+    return new Promise(resolve => {
+        readline.question(message, answer => {
+            resolve(answer)
+        })
+    })
+}
 
 try {
+
     writeFileSync("./test.txt", content)
+
 } catch (err) {
     console.error(err)
 }
