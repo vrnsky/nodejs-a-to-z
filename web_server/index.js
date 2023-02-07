6bf64043be8dd8e25b850bb107ad29451cb3926b
vrnsky <vrnsky@protonmail.ch>
2023-02-07 14:15:11 +0800

    Node JS A to Z. Add simple web server based on Express

---
+++ web_server/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import workingHours from "./data/workingHours.js";
+import menuItems from "./data/menuItems.js";
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.send("Welcome to What's Fare is Fair!")
+})
+
+app.get("/menu", (req, res) => {
+    res.send(menuItems)
+})
+
+app.get("/hours", (req, res) => {
+    res.send(workingHours)
+})
+
+app.listen(port, () => {
+    console.log(`Web Server is listenning at localhost:${port}`)
+})
