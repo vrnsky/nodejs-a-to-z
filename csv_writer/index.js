845c5c33daec2a97b384919f6511375435a6e150
vrnsky <vrnsky@protonmail.ch>
2023-02-07 13:37:34 +0800

    NodeJS A to Z. Full implementation of CSV Writer

--- csv_writer/index.js
+++ csv_writer/index.js
@@ -1,4 +1,5 @@
 import { writeFileSync } from "fs"
+import { appendFileSync } from "fs"
 import { createInterface } from "readline"
 
 const content = "Test content"
@@ -15,10 +16,32 @@ const readLineAsync = message => {
     })
 }
 
-try {
+class Person {
+    constructor(name = "", number = "", email = "") {
+        this.name = name
+        this.number = number
+        this.email = email
+    }
 
-    writeFileSync("./test.txt", content)
-
-} catch (err) {
-    console.error(err)
+    saveToCSV() {
+        const content = `${this.name},${this.number},${this.email}`
+        try {
+            appendFileSync("./contacts.csv", content)
+        } catch (err) {
+            console.error(err)
+        }
+    }
 }
+
+const startApp = async () => {
+    const person = new Person()
+    person.name = await readLineAsync("Contact Name: ")
+    person.number = await readLineAsync("Contact Number: ")
+    person.email = await readLineAsync("Contact Email: ")
+    person.saveToCSV()
+    const response = await readLineAsync("Continue? [y to continue]: ")
+    if (response === "y") await startApp()
+    else readline.close()
+}
+
+startApp()
